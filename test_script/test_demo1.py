2f7ee8d9a5e7132853326b990fd1f2e56c37d1e3
kiran <kiranr.kumar91@gmail.com>
2022-06-01 23:02:29 +0530

    hi git

---
+++ test_script/test_demo1.py
@@ -0,0 +1,11 @@
+from generic.base_setup import Base_Setup
+from generic.utility import Excel
+
+
+class Test_1(Base_Setup):
+
+    def test_1(self):
+        print(self.driver.title)
+        v = Excel.get_data("test_data/data.xlsx", "Sheet1", 1, 1)
+        print("From excel", v)
+        print('Hi hello')
